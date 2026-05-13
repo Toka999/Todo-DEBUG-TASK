d47e3a0654449aa82cc307ab6ec35ff27ca19d6a
Toka999 <tokaibrahim19992gmail.com>
2026-05-13 11:52:56 +0300

    some fixed bugs

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter } from "react-router-dom";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
@@ -5,7 +6,10 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <BrowserRouter>
+    <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
+  </BrowserRouter>
+  
 );
